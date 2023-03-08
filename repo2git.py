4f164c9c613c5411bf607e7b22adfb8bd6dfd21a
solonobita <solomoon1314@gmail.com>
2023-03-08 01:46:44 +0000

    update

--- repo2git.py
+++ repo2git.py
@@ -6,9 +6,7 @@ import sys
 import xml.etree.ElementTree as ET
 import re
 
-# 处理真正的远程url
-
-
+# 处理生成真正的远程url
 def produce_real_repositories(projects: dict, remotes: dict):
     repositories = {}
     for name in projects.keys():
@@ -16,9 +14,9 @@ def produce_real_repositories(projects: dict, remotes: dict):
         remote = project.get('remote')
         url = None
         if is_repo_url(remote):
-            url = remote + '/' + name + '.git'
+            url = remote + '/' + name
         elif remotes.get(remote):
-            url = remotes.get(remote) + '/' + name + '.git'
+            url = remotes.get(remote) + '/' + name
         if not url:
             print('无法找到项目{}的远程下载地址'.format(name))
             return None
@@ -44,7 +42,6 @@ def get_repositories(manifest_file_path: str):
             remote = child.attrib['remote']
             branch = child.attrib.get('upstream', 'master')
             revision = child.attrib.get('revision', 'master')
-
             projects[name] = {
                 'path': path,
                 'remote': remote,
@@ -54,14 +51,10 @@ def get_repositories(manifest_file_path: str):
         elif child.tag == 'remote':
             name = child.attrib['name']
             fetch = child.attrib['fetch']
-
             remotes.update({name: fetch})
-
     return produce_real_repositories(projects, remotes)
 
 # 将 Repo Manifest 中的所有仓库转换为 Git 子模块
-
-
 def convert_to_git_submodules(manifest_file_path: str):
     if not check_file_exist(manifest_file_path):
         print('文件', manifest_file_path, '不存在', '退出解析... ...')
@@ -76,28 +69,19 @@ def convert_to_git_submodules(manifest_file_path: str):
         url = info.get('url')
         revision = info.get('revision')
         branch = info.get('branch')
-
         print(name, path, url, branch, revision)
-        # print()
-        ck_url = r'{}@{}'.format(url, revision)
-
         # 添加 Git 子模块
-        os.system(f'git submodule add -b {branch} --reference {ck_url} {url} {path}')
-
-        # # 拉取子模块的代码
-        # #os.system(f'git submodule update --init --recursive {path}')
-
+        if os.system(f'git submodule add --force -b {branch} {url} {path}') == 0 and os.system(f'git submodule update --init --recursive {path}') == 0 and os.system(f'git -C {path} checkout {revision}') == 0:
+            print('检出{}仓库到branch<{}>的指定revision<{}>成功'.format(name, branch, revision))
+        else:
+            print('检出{}仓库到branch<{}>的指定revision<{}>失败, 退出中... ... ... ...'.format(name, branch, revision))
+            return
+            
         # # 提交子模块
         # os.system(f'git add {path}')
         # os.system(f'git commit -m "Add {name} submodule"')
 
-    # 删除 Repo Manifest
-    #os.system('git rm repo manifest.xml')
-    #os.system('git commit -m "Remove Repo Manifest"')
-
-    print('All repositories have been converted to Git submodules.')
-
-# 调用函数
+    print('所有仓库检出成功！')
 
 
 def check_file_exist(path: str):
@@ -111,7 +95,7 @@ def check_args():
 def help():
     print('usage', sys.argv[0], r'<repo manifest path>')
 
-
+# 检测repo url 是否为映射
 def is_repo_url(url: str):
     pattern = r'^(git|https|http)://.*$'
     return re.match(pattern, url)
@@ -122,6 +106,10 @@ def main():
         convert_to_git_submodules(sys.argv[1])
     else:
         help()
+
+
+def check_result(result: int):
+    print(result)
 
 
 if __name__ == '__main__':
